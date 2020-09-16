ddf7742ce4b3513811c7449daa85c532584a2d60
Arthurmtro <arthurmtro@gmail.com>
2020-09-16 19:40:29 +0100

    adding layout

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"; // eslint-disable-line no-unused-vars
 import Helmet from "react-helmet";
 import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
